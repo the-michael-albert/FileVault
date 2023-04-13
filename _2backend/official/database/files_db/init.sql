2e9d7828c607720cda108fb200e5e76f11b42ab3
MikeAl <michaeltesto15@hotmail.com>
2023-04-13 14:00:19 -0700

    restructured db to work for hibernate object

--- _2backend/official/database/files_db/init.sql
+++ _2backend/official/database/files_db/init.sql
@@ -3,29 +3,31 @@ CREATE DATABASE IF NOT EXISTS vault;
 
 USE vault;
 
+CREATE TABLE IF NOT EXISTS metadata (
+    id BIGINT PRIMARY KEY unique,
+    width SMALLINT,
+    height SMALLINT,
+    duration BIGINT,
+    resource_id CHAR(40) unique
+);
+
+CREATE TABLE IF NOT EXISTS detail (
+    id BIGINT PRIMARY KEY unique,
+    resource_id CHAR(40) unique,
+    file_name VARCHAR(255),
+    FOREIGN KEY (resource_id) REFERENCES metadata(resource_id),
+    FOREIGN KEY (id) REFERENCES metadata(id)
+);
+
 CREATE TABLE IF NOT EXISTS file (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT unique,
     resource_id CHAR(40) UNIQUE,
     date_year SMALLINT,
     date_month TINYINT,
     date_day TINYINT,
-    file_type TINYINT
-);
-
-CREATE TABLE IF NOT EXISTS metadata (
-    id BIGINT PRIMARY KEY,
-    width SMALLINT,
-    height SMALLINT,
-    duration BIGINT,
-    resource_id CHAR(40) UNIQUE,
-    FOREIGN KEY (id) REFERENCES file(id),
-    FOREIGN KEY (resource_id) REFERENCES file(resource_id)
-);
-
-
-CREATE TABLE IF NOT EXISTS detail (
-    id BIGINT PRIMARY KEY,
-    resource_id CHAR(40) UNIQUE,
-    file_name VARCHAR(255),
-    FOREIGN KEY (resource_id) REFERENCES file(resource_id)
+    file_type TINYINT,
+    FOREIGN KEY (id) REFERENCES metadata(id),
+    FOREIGN KEY (id) REFERENCES detail(id),
+    FOREIGN KEY (resource_id) REFERENCES metadata(resource_id),
+    FOREIGN KEY (resource_id) REFERENCES detail(resource_id)
 );
