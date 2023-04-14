1dbcd971d04a08ddbe22f1834345d7291c5f3cda
MikeAl <michaeltesto15@hotmail.com>
2023-04-14 01:06:50 -0700

    made flask server that takes in json and sends email
    also added postman test

--- _2backend/test/email_service/pymail/sendmail.py
+++ _2backend/test/email_service/pymail/sendmail.py
@@ -1,42 +1,52 @@
+import getpass
+
+from flask import Flask, request
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from getpass import getpass
 
+app = Flask(__name__)
 
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    # Get email parameters from JSON request body
+    email_data = request.get_json()
+    subject = email_data['subject']
+    body = email_data['body']
 
-# Email parameters
-from_addr = 'contact@michaelalbert.one'
-to_addr = 'malber@uw.edu'
-subject = 'Test Email'
-body = '<html></html>'
+    # Get email addresses from URL parameters
+    from_addr = email_data['from']
+    to_addr = email_data['to']
 
-# SMTP server configuration
-smtp_server = 'smtp.office365.com'
-smtp_port = 587
-smtp_username = 'contact@michaelalbert.one'
-smtp_password = getpass('Enter your password: ')
+    # SMTP server configuration
+    smtp_server = 'smtp.office365.com'
+    smtp_port = 587
+    smtp_username = 'contact@michaelalbert.one'
+    smtp_password = getpass.getpass("password:")
 
-# Create message container
-msg = MIMEMultipart()
-msg['From'] = from_addr
-msg['To'] = to_addr
-msg['Subject'] = subject
+    # Create message container
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = subject
 
-# Add message body
-msg.attach(MIMEText(body, 'plain'))
+    # Add message body
+    msg.attach(MIMEText(body, email_data['type']))
 
-# Create SMTP session and encrypt with STARTTLS
-smtp_session = smtplib.SMTP(smtp_server, smtp_port)
-smtp_session.starttls()
+    # Create SMTP session and encrypt with STARTTLS
+    smtp_session = smtplib.SMTP(smtp_server, smtp_port)
+    smtp_session.starttls()
 
-# Login to SMTP server
-smtp_session.login(smtp_username, smtp_password)
+    # Login to SMTP server
+    smtp_session.login(smtp_username, smtp_password)
 
-# Send email
-smtp_session.sendmail(from_addr, to_addr, msg.as_string())
+    # Send email
+    smtp_session.sendmail(from_addr, to_addr, msg.as_string())
 
-# Quit SMTP session
-smtp_session.quit()
+    # Quit SMTP session
+    smtp_session.quit()
 
-print('Email sent successfully.')
+    return 'Email sent successfully.'
+
+if __name__ == '__main__':
+    app.run()
