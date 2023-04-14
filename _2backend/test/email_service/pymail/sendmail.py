3966127a024bed8e9d94906d2ab98242dd857b66
MikeAl <michaeltesto15@hotmail.com>
2023-04-14 00:42:44 -0700

    added properly working email sender

--- _2backend/test/email_service/pymail/sendmail.py
+++ _2backend/test/email_service/pymail/sendmail.py
@@ -1,54 +1,42 @@
 import smtplib
-import sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-import getpass
+from getpass import getpass
 
-# Set up the SMTP server
-smtp_server = "smtpout.secureserver.net"
-port = 465 # SSL port
 
-# Set up the login credentials
-username = "contact@michaelalbert.one"
 
-# Set up the email contents
-sender = "contact@michaelalbert.one"
-recipient = "malber@uw.edu"
-subject = "Example HTML email"
-html = """\
-<html>
-  <body>
-    <h1>This is an example HTML email!</h1>
-    <p>Here is some content:</p>
-    <ul>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-    </ul>
-    <img src="cid:image1">
-  </body>
-</html>
-"""
-# Load an image file to attach as an inline image
-with open("image.png", "rb") as f:
-    image_data = f.read()
-image = MIMEImage(image_data)
-image.add_header('Content-ID', '<image1>')
+# Email parameters
+from_addr = 'contact@michaelalbert.one'
+to_addr = 'malber@uw.edu'
+subject = 'Test Email'
+body = '<html></html>'
 
-# Create the message object and set its attributes
+# SMTP server configuration
+smtp_server = 'smtp.office365.com'
+smtp_port = 587
+smtp_username = 'contact@michaelalbert.one'
+smtp_password = getpass('Enter your password: ')
+
+# Create message container
 msg = MIMEMultipart()
-msg['From'] = sender
-msg['To'] = recipient
+msg['From'] = from_addr
+msg['To'] = to_addr
 msg['Subject'] = subject
-msg.attach(MIMEText(html, 'html'))
-msg.attach(image)
 
-print("hi");
+# Add message body
+msg.attach(MIMEText(body, 'plain'))
 
-password = getpass.getpass("pass:", stream=sys.stdin)
-print(password)
-# Send the email
-with smtplib.SMTP_SSL(smtp_server, port) as server:
-    server.login(username, password)
-    server.sendmail(sender, recipient, msg.as_string())
+# Create SMTP session and encrypt with STARTTLS
+smtp_session = smtplib.SMTP(smtp_server, smtp_port)
+smtp_session.starttls()
+
+# Login to SMTP server
+smtp_session.login(smtp_username, smtp_password)
+
+# Send email
+smtp_session.sendmail(from_addr, to_addr, msg.as_string())
+
+# Quit SMTP session
+smtp_session.quit()
+
+print('Email sent successfully.')
