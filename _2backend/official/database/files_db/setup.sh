7b088e92010157e2b17be0460cff4b74c631e839
MikeAl <michaeltesto15@hotmail.com>
2023-04-14 20:45:03 -0700

    docker trying to fix

--- _2backend/official/database/files_db/setup.sh
+++ _2backend/official/database/files_db/setup.sh
@@ -1,2 +1,2 @@
 docker build -t fileserver .
-docker run -p 3306:3306 --name my-mariadb-container -e MYSQL_ROOT_PASSWORD=password -d fileserver
+docker run -p 3306:3306 --name entry-service -e MYSQL_ROOT_PASSWORD=password -d fileserver
