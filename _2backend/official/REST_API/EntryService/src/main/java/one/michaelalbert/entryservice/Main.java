6b6372f17afd7c29084ce06c273a4a7baad73963
MikeAl <michaeltesto15@hotmail.com>
2023-04-15 14:12:08 -0700

    messed up, actually moved file this time

---
+++ _2backend/official/REST_API/EntryService/src/main/java/one/michaelalbert/entryservice/Main.java
@@ -0,0 +1,15 @@
+package one.michaelalbert.entryservice;
+
+import one.michaelalbert.entryservice.commons.FileObject;
+import one.michaelalbert.entryservice.database.DatabaseConnector;
+
+import java.sql.*;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        DatabaseConnector connector = new DatabaseConnector();
+    }
+
+
+}
